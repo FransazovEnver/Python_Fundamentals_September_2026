7aee93f1621de8b4ec20e6f214f6aef3555e4f14
Enver Fransazov <enverfransazov@gmail.com>
2026-09-13 16:33:47 +0300

    feat: refact

--- Basic_Syntax_Conditional_Statements_and_Loops_Lab_10_09_26/largest_of_three_numbers.py
+++ Basic_Syntax_Conditional_Statements_and_Loops_Lab_10_09_26/largest_of_three_numbers.py
@@ -6,5 +6,5 @@ if first_number > second_number and first_number > third_number:
     print(first_number)
 elif second_number > first_number and second_number > third_number:
     print(second_number)
-elif third_number > first_number and third_number > second_number:
+else:
     print(third_number)
